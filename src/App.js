1a1ca11f56ee08d072237dcfd6fb801ce743c7f8
Srikanthch534 <srikanthch5345@gmail.com>
2022-10-14 03:56:26 +0000

    lightDarkMode

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-named-as-default */
+// eslint-disable-next-line import/no-named-as-default-member
+import LightDarkMode from './components/LightDarkMode'
+
+import './App.css'
+
+const App = () => <LightDarkMode />
+
+export default App
